4d3f9b76aad917b92f83b884b769adcb1ed70f13
Juan Chávez <alu0100401165@ull.edu.es>
2013-11-06 09:31:39 +0000

    Implementadas las pruebas las para las diferentes operaciones con matrices

--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -23,3 +23,34 @@ describe Matriz do
       @m3.to_s.should == "2 0 1 \n3 0 0 \n5 1 1 \n"
     end
   end
+  
+  describe "Operaciones unarias" do
+    it "Traspuesta" do
+      (@m3.tras.matriz == Matriz.new([[2,3,5],[0,0,1],[1,0,1]]).matriz).should be_true
+    end
+    it "Determinante" do
+      (@m1.det.should == -6)
+    end
+  end
+
+  describe "Multiplicar por un escalar" do
+    it "Multiplicacion" do
+      ((@m3.x(2)).matriz.should == Matriz.new([[4,0,2],[6,0,0],[10,2,2]]).matriz)
+      ((@m1.x(-3)).matriz.should == Matriz.new([[-3,-9],[-6,0]]).matriz)
+    end
+  end
+
+  describe "Operaciones con dos matrices" do
+    it "Se deben poder sumar dos matrices" do
+      ((@m1 + @m2).matriz.should == Matriz.new([[3,4],[2,1]]).matriz)
+    end
+    it "Se deben poder multiplicar dos matrices" do
+      ((@m1 * @m2).matriz.should == Matriz.new([[2,4],[4,2]]).matriz)
+      ((@m1 * @m4).matriz.should == Matriz.new([[10,1,9],[8,2,12]]).matriz)
+    end
+    it "Se deben poder restar dos matrices" do
+      ((@m1 - @m2).matriz.should == Matriz.new([[-1,2],[2,-1]]).matriz)
+    end
+  end
+
+end
